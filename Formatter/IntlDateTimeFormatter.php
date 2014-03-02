6c4e30403c75db0fae6a6c2ce62f64f9f8765e0a
Soliman CHEYSSIAL <soliman@socle.vm>
2014-03-02 13:34:43 +0100

    Add formatter

---
+++ Formatter/IntlDateTimeFormatter.php
@@ -0,0 +1,349 @@
+<?php
+
+namespace Socle\Bundle\IntlDateTimeFormatBundle\Formatter;
+
+use Socle\Bundle\IntlDateTimeFormatBundle\Handler\LocaleHandlerInterface;
+
+/**
+ * Class IntlDateTimeFormatter
+ * 
+ * @author Soliman Cheyssial <scheyssial@sqli.com>
+ */
+class IntlDateTimeFormatter implements IntlDateTimeFormatterInterface
+{
+    /**
+     * @var integer
+     */
+    protected $dateType = \IntlDateFormatter::MEDIUM;
+
+    /**
+     * @var integer
+     */
+    protected $timeType = \IntlDateFormatter::MEDIUM;
+
+    /**
+     * @var array
+     */
+    protected $localizedFormats;
+
+    /**
+     * Class constructor
+     *
+     * @param LocaleHandlerInterface $localeHandler    The locale handler
+     * @param array                  $localizedFormats The overloaded formats
+     */
+    public function __construct(LocaleHandlerInterface $localeHandler, $localizedFormats)
+    {
+        $this->localeHandler    = $localeHandler;
+        $this->localizedFormats = $localizedFormats;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formatDate($value, $locale = null, $dateType = null, $timeZone = null)
+    {
+        $this->validateType($dateType);
+
+        return $this->getFormattedDateTime($value, $locale, $dateType, 'none', $timeZone);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formatTime($value, $locale = null, $timeType = null, $timeZone = null)
+    {
+        $this->validateType($timeType);
+
+        return $this->getFormattedDateTime($value, $locale, 'none', $timeType, $timeZone);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formatDateTime($value, $locale = null, $dateType = null, $timeType = null, $timeZone = null)
+    {
+        $this->validateType($dateType, $timeType);
+
+        return $this->getFormattedDateTime($value, $locale, $dateType, $timeType, $timeZone);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormattedDateTime($date, $locale = null, $dateType = null, $timeType = null, $timeZone = null)
+    {
+        if ($date === null) {
+            return null;
+        }
+
+        $returningDate = $this->validateString($date);
+
+        if ($returningDate instanceof \DateTime) {
+            // Get timestamp
+            $returningDate = floatval($returningDate->format('U'));
+        }
+
+        $locale = $this->localeHandler->getLocale($locale);
+        $dateTypeToUse = $this->getType($dateType, true);
+        $timeTypeToUse = $this->getType($timeType, true);
+        $timeZone = $this->getTimeZone($timeZone);
+
+        $format = $this->getDateTimeLocaleFormat($locale, $dateType, $timeType);
+        // Get the formatter
+        $formatter = new \IntlDateFormatter(
+            $locale, $dateTypeToUse, $timeTypeToUse, $timeZone, \IntlDateFormatter::GREGORIAN, $format
+        );
+
+        // Get date formatted
+        $result = $formatter->format($returningDate);
+        if (false === $result) {
+            throw new \InvalidArgumentException(sprintf('The value "%s" of type %s cannot be formatted. Error: "%s".', $date, gettype($date), $formatter->getErrorMessage()));
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTimeZone($timeZone)
+    {
+        if (null === $timeZone) {
+            $timeZone = new \DateTimeZone(date_default_timezone_get());
+        } elseif (!($timeZone instanceof \DateTimeZone)) {
+            try {
+                $timeZone = new \DateTimeZone($timeZone);
+            } catch (\Exception $e) {
+                throw new \InvalidArgumentException(sprintf('The "%s" value is not a supported time zone.', $timeZone));
+            }
+        }
+
+        return $timeZone->getName();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateFormatterFormat($format)
+    {
+        switch (strtoupper($format)) {
+            case 'NONE':
+                return \IntlDateFormatter::NONE;
+            case 'FULL':
+                return \IntlDateFormatter::FULL;
+            case 'LONG':
+                return \IntlDateFormatter::LONG;
+            case 'MEDIUM':
+                return \IntlDateFormatter::MEDIUM;
+            case 'SHORT':
+                return \IntlDateFormatter::SHORT;
+            default:
+                throw new \InvalidArgumentException(sprintf('The "%s" value is not a supported format.', $format));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTypeFormat($locale, $type, $key, $localizedFormats = null)
+    {
+        if (!isset($localizedFormats[$locale][$key]) || !isset($localizedFormats[$locale][$key][$type])) {
+            return null;
+        }
+
+        // Verify if the type exists
+        $this->getDateFormatterFormat($type);
+
+        return $localizedFormats[$locale][$key][$type];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isLocaleSurcharged($locale = null, $localizedFormats = null)
+    {
+        if (null === $localizedFormats) {
+            $localizedFormats = $this->localizedFormats;
+        }
+        if (!is_array($localizedFormats) || !isset($localizedFormats[$locale])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLocaleSurchargedDatetimeFormat($locale = null, $dateType = null, $timeType = null, $localizedFormats = null)
+    {
+        if (null === $localizedFormats) {
+            $localizedFormats = $this->localizedFormats;
+        }
+        $locale = $this->localeHandler->getLocale($locale);
+
+        $dateType = $this->getType($dateType);
+        $timeType = $this->getType($timeType);
+        $dateFormat = null;
+        $timeFormat = null;
+
+        if ('none' !== strtolower($dateType)) {
+            $dateFormat = $this->getTypeFormat($locale, $dateType, 'date', $localizedFormats);
+            if (empty($dateFormat)) {
+                $dateFormat = $this->getDefaultLocalizedFormat($locale, $dateType, 'none');
+            }
+        }
+        if ('none' !== strtolower($timeType)) {
+            $timeFormat = $this->getTypeFormat($locale, $timeType, 'time', $localizedFormats);
+            if (empty($timeFormat)) {
+                $timeFormat = $this->getDefaultLocalizedFormat($locale, 'none', $timeType);
+            }
+        }
+
+        if (empty($dateFormat)) {
+            return $timeFormat;
+        } elseif (empty($timeFormat)) {
+            return $dateFormat;
+        }
+
+        return $dateFormat . ' ' . $timeFormat;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateLocaleFormat($locale = null, $dateType = null, $alphaOnly = false)
+    {
+        return $this->getDateTimeLocaleFormat($locale, $dateType, 'none', $alphaOnly);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTimeLocaleFormat($locale = null, $timeType = null, $alphaOnly = false)
+    {
+        return $this->getDateTimeLocaleFormat($locale, 'none', $timeType, $alphaOnly);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateTimeLocaleFormat($locale = null, $dateType = null, $timeType = null, $alphaOnly = false)
+    {
+        $this->validateType($dateType, $timeType);
+
+        $format = $this->getLocalizedDateTimeFormat($locale, $dateType, $timeType);
+        if ($alphaOnly) {
+            $format = preg_replace('#[^a-zA-Z]+#', '', $format);
+        }
+
+        return $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLocalizedDateTimeFormat($locale = null, $dateType = null, $timeType = null)
+    {
+        $locale = $this->localeHandler->getLocale($locale);
+
+        if (strlen($locale) > 2) {
+            $locale = substr($locale, 0, 2);
+        }
+
+        $localizedFormats = $this->localizedFormats;
+
+        if ($this->isLocaleSurcharged($locale, $localizedFormats)) {
+            $format = $this->getLocaleSurchargedDatetimeFormat($locale, $dateType, $timeType, $localizedFormats);
+        } else {
+            $format = $this->getDefaultLocalizedFormat($locale, $dateType, $timeType);
+        }
+
+        return $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultLocalizedFormat($locale = null, $dateType = null, $timeType = null)
+    {
+        $dateTypeToUse = $this->getType($dateType, true);
+        $timeTypeToUse = $this->getType($timeType, true);
+
+        $formatter = new \IntlDateFormatter(
+            $locale, $dateTypeToUse, $timeTypeToUse, null, \IntlDateFormatter::GREGORIAN
+        );
+
+        return $formatter->getPattern();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType($type = null, $formatter = false)
+    {
+        if (empty($type)) {
+            $type = static::DEFAULT_TYPE;
+        }
+
+        if ($formatter) {
+            return $this->getDateFormatterFormat($type);
+        }
+
+        return $type;
+    }
+
+    /**
+     * Validate one or two format types
+     *
+     * @param string         $type       The type to validate
+     * @param string|boolean $secondType If not false, the second type to validate
+     *
+     * @return boolean
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function validateType($type, $secondType = false)
+    {
+        if ('none' === $type) {
+            if (false !== $secondType) {
+                $this->validateType($secondType);
+            } else {
+                throw new \InvalidArgumentException(sprintf('The "%s" value is not a supported type.', $type));
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Validate and transform date value if "$date" is a string
+     *
+     * @param mixed $date The date value to validate
+     *
+     * @return \DateTime|float
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function validateString($date)
+    {
+        $returningDate = $date;
+        // If given date is string
+        if (is_string($returningDate)) {
+            // Check whether the string contains only digits
+            if (ctype_digit($returningDate)) {
+                $returningDate = floatval($returningDate);
+            } else {
+                try {
+                    $returningDate = new \DateTime($returningDate);
+                } catch (\Exception $e) {
+                    throw new \InvalidArgumentException(
+                        sprintf('The value "%s" of type %s is invalid. Error: "%s".', $date, gettype($date), $e->getMessage()));
+                }
+            }
+        }
+
+        return $returningDate;
+    }
+}
---
+++ Formatter/IntlDateTimeFormatterInterface.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Socle\Bundle\IntlDateTimeFormatBundle\Formatter;
+
+/**
+ * Class IntlDateTimeFormatterInterface
+ * 
+ * @author Soliman Cheyssial <scheyssial@sqli.com>
+ */
+interface IntlDateTimeFormatterInterface
+{
+    const DEFAULT_TYPE = 'medium';
+
+    /**
+     * Formats a timestamp as date
+     *
+     * @param mixed  $value    Date value to be formatted
+     * @param string $locale   Locale to be used with {@link http://php.net/manual/class.intldateformatter.php}
+     * @param string $dateType Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeZone Time zone from {@link http://php.net/manual/timezones.php}
+     *
+     * @return string Formatted date
+     */
+    function formatDate($value, $locale = null, $dateType = null, $timeZone = null);
+
+    /**
+     * Formats a timestamp as time
+     *
+     * @param mixed  $value    Time value to be formatted
+     * @param string $locale   Locale to be used with {@link http://php.net/manual/class.intldateformatter.php}
+     * @param string $timeType Time format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeZone Time zone from {@link http://php.net/manual/timezones.php}
+     *
+     * @return string Formatted time
+     */
+    function formatTime($value, $locale = null, $timeType = null, $timeZone = null);
+
+    /**
+     * Formats a timestamp as date and time
+     *
+     * @param mixed  $value    Date/time value to be formatted
+     * @param string $locale   Locale to be used with {@link http://php.net/manual/class.intldateformatter.php}
+     * @param string $dateType Date format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeType Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeZone Time zone from {@link http://php.net/manual/timezones.php}
+     *
+     * @return string Formatted date and time
+     *
+     */
+    function formatDateTime($value, $locale = null, $dateType = null, $timeType = null, $timeZone = null);
+
+    /**
+     * Format a date according to locale
+     *
+     * @param \DateTime|string $date     DateTime value to be formatted
+     * @param string           $locale   The locale from which getting format
+     * @param string           $dateType Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string           $timeType Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param string           $timeZone Time zone from {@link http://php.net/manual/timezones.php}
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    function getFormattedDateTime($date, $locale = null, $dateType = null, $timeType = null, $timeZone = null);
+
+    /**
+     * Get timezone
+     *
+     * @param mixed $timeZone The timezone
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    function getTimeZone($timeZone);
+
+    /**
+     * Get format type
+     *
+     * @param string $format Date/time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     *
+     * @return integer Appropriate value of {@link http://php.net/manual/class.intldateformatter.php#intl.intldateformatter-constants}
+     *
+     * @throws \InvalidArgumentException
+     */
+    function getDateFormatterFormat($format);
+
+    /**
+     * Get the format value for the key type
+     *
+     * @param string $locale           The locale to search
+     * @param string $type             The type to get
+     * @param string $key              The key (date or time) to get
+     * @param array  $localizedFormats The format table
+     *
+     * @return string
+     */
+    function getTypeFormat($locale, $type, $key, $localizedFormats = null);
+
+    /**
+     * Check if the given locale is surcharged
+     *
+     * @param string $locale           The locale to search
+     * @param array  $localizedFormats The format table
+     *
+     * @return boolean
+     */
+    function isLocaleSurcharged($locale = null, $localizedFormats = null);
+
+    /**
+     * Get format for surcharged locale
+     *
+     * @param string $locale           The locale from which getting format
+     * @param string $dateType         Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeType         Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param array  $localizedFormats The format table
+     *
+     * @return string
+     */
+    function getLocaleSurchargedDatetimeFormat($locale = null, $dateType = null, $timeType = null, $localizedFormats = null);
+
+    /**
+     * Get date format for given locale
+     *
+     * @param string  $locale    Locale to be used with {@link http://php.net/manual/class.intldateformatter.php}
+     * @param string  $dateType  Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param boolean $alphaOnly If format must be only alphanumerical. Ex: dd/MM/yyyy => ddMMyyyy
+     *
+     * @return string
+     */
+    public function getDateLocaleFormat($locale = null, $dateType = null, $alphaOnly = false);
+
+    /**
+     * Get time format for given locale
+     *
+     * @param string  $locale    Locale to be used with {@link http://php.net/manual/class.intldateformatter.php}
+     * @param string  $timeType  Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param boolean $alphaOnly If format must be only alphanumerical. Ex: HH:mm:ss => HHmmss
+     *
+     * @return string
+     */
+    public function getTimeLocaleFormat($locale = null, $timeType = null, $alphaOnly = false);
+
+    /**
+     * Get datetime format for given locale
+     *
+     * @param string  $locale    The locale from which getting format
+     * @param string  $dateType  Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string  $timeType  Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     * @param boolean $alphaOnly If format must be only alphanumerical. Ex: dd/MM/yyyy HH:mm:ss => ddMMyyyyHHmmss
+     *
+     * @return string
+     */
+    public function getDateTimeLocaleFormat($locale = null, $dateType = null, $timeType = null, $alphaOnly = false);
+
+    /**
+     * Get datetime format for given locale
+     *
+     * @param string $locale   The locale from which getting format
+     * @param string $dateType Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeType Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     *
+     * @return string
+     */
+    public function getLocalizedDateTimeFormat($locale = null, $dateType = null, $timeType = null);
+
+    /**
+     * Get default datetime format for given locale
+     *
+     * @param string $locale   The locale from which getting format
+     * @param string $dateType Date format. Valid values are "full", "long", "medium", or "short" (case insensitive)
+     * @param string $timeType Time format. Valid values are "none", "full", "long", "medium", or "short" (case insensitive)
+     *
+     * @return string
+     */
+    public function getDefaultLocalizedFormat($locale = null, $dateType = null, $timeType = null);
+
+    /**
+     * Get default type if not defined
+     *
+     * @param string  $type      The original type
+     * @param boolean $formatter Whether to use formatter format
+     *
+     * @return string
+     */
+    function getType($type = null, $formatter = false);
+} 
